33e23cd1fb8f6b5b64a585db86b16bc88debd76a
danishanr <danishaanr@gmail.com>
2025-06-05 15:02:43 +0700

    Membuat method push

--- stack.cpp
+++ stack.cpp
@@ -20,11 +20,23 @@ class Stack
 {
 private :
     Node *top; // Pointer to the top of the node stack
-    
+
 public :
     Stack() 
     {
         top = NULL; // Initialize the stack with a null top pointer
     }
+    
+    // Push opertation: Insert an element onto the top of the stack
+    int push(int value)
+    {
+        Node *newNode = new Node();     // 1. Allocate memory for the new node
+        newNode->data = value;          // 2. Assign value
+        newNode->next = top;            // 3. Set the pointer of the new node to the current
+        top = newNode;                  // 4. Update the top pointer to the new node
+        cout << "Push value : " << value << endl;
+        return value;
+    }
+
 
 };
